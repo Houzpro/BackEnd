2141e04a9af0c6bc8752c43ad11a1d0229f75930
Houzpro <degorkosterev@gmail.com>
2024-12-19 20:36:58 +0300

    upd

--- main.go
+++ main.go
@@ -206,7 +206,7 @@ type Book struct {
 }
 
 func initDB() {
-	dsn := "host=localhost user=postgres password=qwe123993 dbname=Backend_Prak port=5432 sslmode=disable"
+	dsn := "host=localhost user=postgres password=Qwe123993 dbname=Backend_Prak port=5432 sslmode=disable"
 	var err error
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
